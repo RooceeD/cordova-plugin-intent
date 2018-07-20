1dea39cb796fcc8366bf1cc3b6db42936e4832e6
Paul <RooceeD@users.noreply.github.com>
2018-07-20 13:50:29 +0200

    Update IntentPlugin.java

--- src/android/IntentPlugin.java
+++ src/android/IntentPlugin.java
@@ -258,14 +258,6 @@ public class IntentPlugin extends CordovaPlugin {
         }
     }
     
-    public String getRealPathFromURI(Uri contentUri) {
-        String[] proj = { MediaStore.Images.Media.DATA };
-        Cursor cursor = this.cordova.getActivity().getApplicationContext().getContentResolver().query(contentUri, proj, null, null, null);
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        return cursor.getString(column_index);
-    }
-    
     public String getFilePathFromURI(Uri contentUri) {
         Context context = this.cordova.getActivity().getApplicationContext();
         String fileName = getFileName(contentUri);
